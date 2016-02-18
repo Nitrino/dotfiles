401c5cd0106cc65ef0b137723460cf13c416be8e
Petr Stepchenko <nitrinodev@gmail.com>
2016-02-19 02:27:28 +0700

    add aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,36 @@
+# git
+alias gc="git commit -m"
+alias gcl='git clone'
+alias gr='git rebase'
+alias gplr='git pull --rebase'
+alias gps='git push'
+alias gpsh='git push -u origin'
+alias gs='git status'
+alias ga='git add'
+
+# cd
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias cdnginx='cd /usr/local/etc/nginx/'
+alias cdphp='cd /usr/local/etc/php/7.0/'
+
+# ps
+alias psa="ps aux"
+alias psg="ps aux | grep "
+
+# common shell functions
+alias reload!='. ~/.zshrc'
+alias tf='tail -f'
+alias cl='clear'
+alias ka9='killall -9'
+alias k9='kill -9'
+
+# ruby
+alias rc='rails c'
+alias rs='rails s'
+alias rdm='rake db:migrate'
+alias rdr='rake db:rollback'
+
+# utils
+alias weather='curl -4 http://wttr.in/'
