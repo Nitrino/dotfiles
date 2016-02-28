cf7899311552c829322fb3842bebc7df3c6cf6aa
Petr Stepchenko <nitrinodev@gmail.com>
2016-02-28 18:34:00 +0700

    add exports

---
+++ zsh/exports.zsh
@@ -0,0 +1,4 @@
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$(brew --prefix homebrew/php/php70)/bin:$PATH"
+export PATH="$PATH:/Users/nitrino/Library/Android/sdk/platform-tools"
