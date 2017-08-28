6083ae00c8c7cecf8e6aa4b93fb555299ba2e6d9
Petr Stepchenko <nitrinodev@gmail.com>
2017-08-28 13:34:57 +0300

    clear exports

--- zsh/oh-my-zsh/custom/exports.zsh
+++ zsh/oh-my-zsh/custom/exports.zsh
@@ -1,7 +1,5 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-export PATH="$PATH:$HOME/.rvm/bin"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="$(brew --prefix homebrew/php/php70)/bin:$PATH"
 export PATH="$PATH:/Users/nitrino/Library/Android/sdk/platform-tools"
 export PYENV_ROOT=/usr/local/var/pyenv
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
