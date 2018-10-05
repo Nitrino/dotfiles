3c3ca57fbe40e6cdb838856e026c86b522efe147
Petr Stepchenko <nitrinodev@gmail.com>
2018-10-05 18:47:18 +0300

    Update exports

--- zsh/oh-my-zsh/custom/exports.zsh
+++ zsh/oh-my-zsh/custom/exports.zsh
@@ -1,6 +1,14 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$PATH:/Users/nitrino/Library/Android/sdk/platform-tools"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/bin/elixir:$PATH"
+export PATH="/usr/local/mysql-5.7.23-macos10.13-x86_64/bin:$PATH"
+
 export PYENV_ROOT=/usr/local/var/pyenv
+export GPG_TTY=$(tty)
+export TEST_ENV_NUMBER='2'
+
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+export ERL_AFLAGS="-kernel shell_history enabled"
